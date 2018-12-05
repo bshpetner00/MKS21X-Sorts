6c7a31fa03b53d073fc22a3b9ce1d6d77e105e14
bshpetner00 <bshpetner00@stuy.edu>
2018-12-05 09:53:57 -0500

    ran some more tests, made minor change now everything wokrs

--- Sorts.java
+++ Sorts.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
-public class selectionsort {
+public class Sorts {
 	public static void swap(int[]ary, int f, int l) {
 		int temp = ary[f];
 		ary[f] = ary[l];
 		ary[l] = temp;
 	}
-	public static void selectionsort(int [] ary) {
+	public static void selectionSort(int [] ary) {
 		for (int i = 0; i < ary.length; i++) {
 			int lowIndex = i;
 			for (int j = i; j < ary.length; j++) {

422f09af844cff14885f009d1ff850eb4e83a164
bshpetner00 <bshpetner00@stuy.edu>
2018-12-07 10:10:53 -0500

    finished and tested bubblesort

--- Sorts.java
+++ Sorts.java
@@ -6,14 +6,32 @@ public class Sorts {
 		ary[l] = temp;
 	}
 	public static void selectionSort(int [] ary) {
-		for (int i = 0; i < ary.length; i++) {
-			int lowIndex = i;
-			for (int j = i; j < ary.length; j++) {
-				if (ary[j] < ary[lowIndex]) {
-					lowIndex = j;
+		if (ary.length != 0) {
+			for (int i = 0; i < ary.length; i++) {
+				int lowIndex = i;
+				for (int j = i; j < ary.length; j++) {
+					if (ary[j] < ary[lowIndex]) {
+						lowIndex = j;
+					}
+				}
+				swap(ary,lowIndex,i);
+			}
+		}
+	}
+	public static void bubbleSort(int [] ary) {
+		if (ary.length != 0) {
+			for (int i = ary.length - 1; i > 0; i--) {
+				for (int j = 0; j < i - 1; j++) {
+					if (ary[j] > ary[i]) {
+						swap(ary,j,i);
+					}
 				}
 			}
-			swap(ary,lowIndex,i);
 		}
+	}
+	public static void main(String[]args) {
+		int[]ary = {70,60,50,40,10};
+		bubbleSort(ary);
+		System.out.println(Arrays.toString(ary));
 	}
 }
